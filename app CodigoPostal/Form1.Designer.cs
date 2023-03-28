21b292104fafe7d77e1f98173d416988cf33d7bf
lpena <lpena@LIZBETH>
2023-03-28 10:26:30 -0600

    Agregar archivos de proyecto.

---
+++ app CodigoPostal/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace app_CodigoPostal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.lstvDatos = new System.Windows.Forms.ListView();
+            this.btnGuardarconInterop = new System.Windows.Forms.Button();
+            this.btnGuardarconOpenxml = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.BtnGraficar = new System.Windows.Forms.Button();
+            this.BtnabrirArbol = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(12, 12);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click_1);
+            // 
+            // lstvDatos
+            // 
+            this.lstvDatos.HideSelection = false;
+            this.lstvDatos.Location = new System.Drawing.Point(12, 41);
+            this.lstvDatos.Name = "lstvDatos";
+            this.lstvDatos.Size = new System.Drawing.Size(418, 366);
+            this.lstvDatos.TabIndex = 1;
+            this.lstvDatos.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnGuardarconInterop
+            // 
+            this.btnGuardarconInterop.Location = new System.Drawing.Point(112, 12);
+            this.btnGuardarconInterop.Name = "btnGuardarconInterop";
+            this.btnGuardarconInterop.Size = new System.Drawing.Size(116, 23);
+            this.btnGuardarconInterop.TabIndex = 3;
+            this.btnGuardarconInterop.Text = "Guardar con Interop";
+            this.btnGuardarconInterop.UseVisualStyleBackColor = true;
+            this.btnGuardarconInterop.Click += new System.EventHandler(this.btnGuardarconInterop_Click);
+            // 
+            // btnGuardarconOpenxml
+            // 
+            this.btnGuardarconOpenxml.Location = new System.Drawing.Point(247, 12);
+            this.btnGuardarconOpenxml.Name = "btnGuardarconOpenxml";
+            this.btnGuardarconOpenxml.Size = new System.Drawing.Size(137, 23);
+            this.btnGuardarconOpenxml.TabIndex = 4;
+            this.btnGuardarconOpenxml.Text = "Guardar con OpenXml";
+            this.btnGuardarconOpenxml.UseVisualStyleBackColor = true;
+            this.btnGuardarconOpenxml.Click += new System.EventHandler(this.btnGuardarconOpenxml_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(854, 495);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 5;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(436, 262);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(390, 225);
+            this.treeView1.TabIndex = 6;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.IntervalOffset = 1D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(436, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(320, 244);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // BtnGraficar
+            // 
+            this.BtnGraficar.Location = new System.Drawing.Point(762, 12);
+            this.BtnGraficar.Name = "BtnGraficar";
+            this.BtnGraficar.Size = new System.Drawing.Size(93, 23);
+            this.BtnGraficar.TabIndex = 8;
+            this.BtnGraficar.Text = "Graficar";
+            this.BtnGraficar.UseVisualStyleBackColor = true;
+            this.BtnGraficar.Click += new System.EventHandler(this.BtnGraficar_Click);
+            // 
+            // BtnabrirArbol
+            // 
+            this.BtnabrirArbol.Location = new System.Drawing.Point(832, 412);
+            this.BtnabrirArbol.Name = "BtnabrirArbol";
+            this.BtnabrirArbol.Size = new System.Drawing.Size(93, 23);
+            this.BtnabrirArbol.TabIndex = 9;
+            this.BtnabrirArbol.Text = "Abrir Arbol";
+            this.BtnabrirArbol.UseVisualStyleBackColor = true;
+            this.BtnabrirArbol.Click += new System.EventHandler(this.BtnabrirArbol_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(762, 61);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(93, 23);
+            this.BtnLimpiar.TabIndex = 10;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 530);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.BtnabrirArbol);
+            this.Controls.Add(this.BtnGraficar);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnGuardarconOpenxml);
+            this.Controls.Add(this.btnGuardarconInterop);
+            this.Controls.Add(this.lstvDatos);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.ListView lstvDatos;
+        private System.Windows.Forms.Button btnGuardarconInterop;
+        private System.Windows.Forms.Button btnGuardarconOpenxml;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button BtnGraficar;
+        private System.Windows.Forms.Button BtnabrirArbol;
+        private System.Windows.Forms.Button BtnLimpiar;
+    }
+}
+
---
+++ app CodigoPostal/Form1.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Controls;
+using Microsoft.Office.Interop.Excel;
+using DocumentFormat.OpenXml;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Security.Cryptography;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace app_CodigoPostal
+{
+    public partial class Form1 : Form
+    {
+        bool maximizado;
+        public Form1()
+        {
+            InitializeComponent();
+            lstvDatos.View = View.Details;
+            maximizado = false;
+        }
+        private void btnAbrir_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogo = new OpenFileDialog();
+            if (dialogo.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            lstvDatos.Clear();
+            string rutaArchivo = dialogo.FileName;
+            StreamReader sr = new StreamReader(rutaArchivo, Encoding.GetEncoding(1252));
+            string columnas = sr.ReadLine();
+            string[] columna = columnas.Split('|');
+            for (int i = 0; i < columna.Length; i++)
+            {
+                lstvDatos.Columns.Add(columna[i]);
+            }
+            string renglon;
+            while ((renglon = sr.ReadLine()) != null)
+            {
+                string[] datos = renglon.Split('|');
+                System.Windows.Forms.ListViewItem item = new System.Windows.Forms.ListViewItem(datos[0]);
+                for (int i = 1; i < datos.Length; i++)
+                {
+                    item.SubItems.Add(datos[i]);
+                }
+                lstvDatos.Items.Add(item);
+            }
+            sr.Close();
+        }
+        private void btnGuardarconInterop_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Archivos de Excel (*.xlsx)|*.xlsx";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Crea una instancia de la aplicación Excel
+                var excelApp = new Microsoft.Office.Interop.Excel.Application();
+                // Crea un nuevo libro de Excel
+                var workbook = excelApp.Workbooks.Add();
+                // Crea una hoja de Excel
+                var worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets[1];
+                // Exporta el contenido del ListView a la hoja de Excel
+                for (int i = 0; i < lstvDatos.Columns.Count; i++)
+                {
+                    worksheet.Cells[1, i + 1] = lstvDatos.Columns[i].Text;
+                }
+                for (int i = 0; i < lstvDatos.Items.Count; i++)
+                {
+                    for (int j = 0; j < lstvDatos.Columns.Count; j++)
+                    {
+                        worksheet.Cells[i + 2, j + 1] = lstvDatos.Items[i].SubItems[j].Text;
+                    }
+                }
+                // Guarda el archivo de Excel
+                workbook.SaveAs(saveFileDialog.FileName);
+                // Cierra el archivo y la aplicación Excel
+                workbook.Close();
+                excelApp.Quit();
+            }
+        }
+        private void btnGuardarconOpenxml_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel files (.xlsx)|.xlsx|All files (.)|.";
+            if (sfd.ShowDialog() != DialogResult.OK)
+            { return; }
+            if (sfd.FileName != "")
+            {
+                SpreadsheetDocument document = SpreadsheetDocument.Create(sfd.FileName, SpreadsheetDocumentType.Workbook);
+                // Agregar una hoja de trabajo
+                WorkbookPart workbookPart = document.AddWorkbookPart();
+                workbookPart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
+                WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                worksheetPart.Worksheet = new DocumentFormat.OpenXml.Spreadsheet.Worksheet(new SheetData());
+                DocumentFormat.OpenXml.Spreadsheet.Sheets sheets = workbookPart.Workbook.AppendChild(new DocumentFormat.OpenXml.Spreadsheet.Sheets());
+                Sheet sheet = new Sheet() { Id = workbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "ListView" };
+                sheets.Append(sheet);
+                // Obtener la colección de celdas
+                SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+                // Recorrer las columnas y filas de la ListView
+                for (int i = 0; i < lstvDatos.Columns.Count; i++)
+                {
+                    // Crear una fila para los encabezados
+                    if (i == 0)
+                    {
+                        Row row = new Row() { RowIndex = 1 };
+                        sheetData.Append(row);
+                    }
+                    // Obtener el encabezado de la columna
+                    string headerText = lstvDatos.Columns[i].Text;
+                    // Crear una celda para el encabezado
+                    Cell headerCell = new Cell()
+                    {
+                        CellReference = GetColumnName(i + 1) + "1",
+                        DataType = CellValues.String,
+                        CellValue = new CellValue(headerText)
+                    };
+                    // Agregar la celda al final de la fila
+                    Row headerRow = (Row)sheetData.ChildElements.GetItem(0);
+                    headerRow.AppendChild(headerCell);
+                    // Recorrer las filas de la columna
+                    for (int j = 0; j < lstvDatos.Items.Count; j++)
+                    {
+                        // Crear una fila para los datos
+                        if (i == 0)
+                        {
+                            Row row = new Row() { RowIndex = (uint)(j + 2) };
+                            sheetData.Append(row);
+                        }
+                        // Obtener el valor del dato
+                        string dataText = lstvDatos.Items[j].SubItems[i].Text;
+                        // Crear una celda para el dato
+                        Cell dataCell = new Cell()
+                        {
+                            CellReference = GetColumnName(i + 1) + (j + 2),
+                            DataType = CellValues.String,
+                            CellValue = new CellValue(dataText)
+                        };
+                        // Agregar la celda al final de la fila
+                        Row dataRow = (Row)sheetData.ChildElements.GetItem(j + 1);
+                        dataRow.AppendChild(dataCell);
+                    }
+                }
+                // Guardar y cerrar el documento
+                workbookPart.Workbook.Save();
+                document.Close();
+                Process.Start(sfd.FileName);
+            }
+        }
+        // Método auxiliar para obtener el nombre de la columna según su índice
+        private string GetColumnName(int index)
+        {
+            int dividend = index;
+            string columnName = String.Empty;
+            int modulo;
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            return columnName;
+        }
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void BtnGraficar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogo = new OpenFileDialog();
+            if (dialogo.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            StreamReader sr = new StreamReader(dialogo.FileName, Encoding.GetEncoding(1252));
+            string renglon;
+            string x = "";
+            int y = 0;
+            while ((renglon = sr.ReadLine()) != null)
+            {
+                string[] datos = renglon.Split('|');
+                if (x != datos[0] && x != "")
+                {
+                    chart1.Series[0].Points.AddXY(x, y);
+                    y = 0;
+                }
+                x = datos[0];
+                y++;
+            }
+            chart1.Series[0].Points.AddXY(x, y);
+        }
+        private void LlenarArbol(StreamReader sr)
+        {
+            string renglon;
+            string x = "";
+            TreeNode ciudad = new TreeNode();
+            TreeNode estado = new TreeNode();
+            TreeNode codigoPostal = new TreeNode();
+            TreeNode colonia = new TreeNode();
+            while ((renglon = sr.ReadLine()) != null)
+            {
+                string[] datos = renglon.Split('|');
+                if (colonia.Text != datos[1] && x != "")
+                {
+                    if (codigoPostal.Text != datos[0] && ciudad.Text != "")
+                    {
+                        if (ciudad.Text != datos[5] && ciudad.Text != "")
+                        {
+                            if (estado.Text != datos[4] && estado.Text != "")
+                            {
+                                treeView1.Nodes.Add(estado.Text);
+                                estado = new TreeNode();
+
+                            }
+                        }
+                        estado.Text = datos[5];
+                        estado.Nodes.Add(ciudad);
+                        ciudad = new TreeNode();
+                    }
+                    ciudad.Text = datos[1];
+                    codigoPostal.Nodes.Add(ciudad);
+                    codigoPostal = new TreeNode();
+                }
+                codigoPostal.Text = datos[5];
+                codigoPostal.Nodes.Add(codigoPostal);
+                colonia.Text = datos[0];
+
+            }
+        }
+        private void BtnabrirArbol_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != DialogResult.OK) { return; }
+            filltree(ofd.FileName);
+
+        }
+      
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            if (maximizado)
+            {
+                chart1.Location = new System.Drawing.Point(436, 12);
+                chart1.Height =240; chart1.Width = 320;
+            }
+            else
+            {
+                chart1.Location = new System.Drawing.Point(0, 0);
+                chart1.Height = this.Height; chart1.Width = this.Width;
+            }
+            maximizado = !maximizado;
+        }
+        private void filltree(string filename)
+        {
+            Dictionary<string, List<string>> cpColonias = new Dictionary<string, List<string>>();
+            string[] lineas = File.ReadAllLines(filename);
+            foreach (string linea in lineas)
+            {
+                string[] campos = linea.Split('|');
+                string estado = campos[4];
+                string ciudad = campos[5];
+                string cp = campos[0];
+                string colonia = campos[1];
+                if (!cpColonias.ContainsKey(cp))
+                {
+                    cpColonias.Add(cp, new List<string>());
+                }
+                cpColonias[cp].Add(colonia);
+                TreeNode estadoNode = null;
+                foreach (TreeNode node in treeView1.Nodes)
+                {
+                    if (node.Text == estado)
+                    {
+                        estadoNode = node;
+                        break;
+                    }
+                }
+                if (estadoNode == null)
+                {
+                    estadoNode = new TreeNode(estado);
+                    treeView1.Nodes.Add(estadoNode);
+                }
+                TreeNode ciudadNode = null;
+                foreach (TreeNode node in estadoNode.Nodes)
+                {
+                    if (node.Text == ciudad)
+                    {
+                        ciudadNode = node;
+                        break;
+                    }
+                }
+                if (ciudadNode == null)
+                {
+                    ciudadNode = new TreeNode(ciudad);
+                    estadoNode.Nodes.Add(ciudadNode);
+                }
+                TreeNode cpNode = null;
+                foreach (TreeNode node in ciudadNode.Nodes)
+                {
+                    if (node.Text == cp)
+                    {
+                        cpNode = node;
+                        break;
+                    }
+                }
+                if (cpNode == null)
+                {
+                    cpNode = new TreeNode(cp);
+                    ciudadNode.Nodes.Add(cpNode);
+                }
+                TreeNode coloniaNode = null;
+                foreach (TreeNode node in cpNode.Nodes)
+                {
+                    if (node.Text == colonia)
+                    {
+                        coloniaNode = node;
+                        break;
+                    }
+                }
+                if (coloniaNode == null)
+                {
+                    coloniaNode = new TreeNode(colonia);
+                    cpNode.Nodes.Add(coloniaNode);
+                }
+            }
+        }
+        private void BtnLimpiar_Click_1(object sender, EventArgs e)
+           {
+            chart1.Series.Clear();
+           }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
